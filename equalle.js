66167220aed787d3c94ca9f0fa072759800382ac
matt j. sorenson <matt@sorensonbros.net>
2012-09-26 14:11:09 -0500

    combining equals sign below: a legal variable in javascript

---
+++ equalle.js
@@ -0,0 +1,9 @@
+var λ͇ = function() {
+  return {
+    equalle : function(s) {
+      document.write(s);
+    }
+  };
+}
+
+λ͇().equalle('<kbd>combining equalle sign belowwe (<a href="http://codepoints.net/U+0347">U+0347</a>)</kbd>');
